1a6dbbab07124e5dca1d2f601aaaf73a5f4c9564
richie.buckingham <richie.buckingham@gmail.com>
2018-08-05 15:58:58 +1000

    tour start date must be before tour end data - server side validation with IValidateableObject

--- TourManagement/TourManagement.API/Dtos/Tours/TourAbstractBase.cs
+++ TourManagement/TourManagement.API/Dtos/Tours/TourAbstractBase.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TourManagement.API.Dtos
 {
-    public abstract class TourAbstractBase
+    public abstract class TourAbstractBase : IValidatableObject
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "Title is required.")]
         [MaxLength(200, ErrorMessage = "Title is too long.")]
@@ -14,5 +14,15 @@ namespace TourManagement.API.Dtos
         public string Description { get; set; }
         public DateTimeOffset StartDate { get; set; }
         public DateTimeOffset EndDate { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!(StartDate < EndDate))
+            {
+                yield return new ValidationResult(
+                "The start date should be smaller than the end date.",
+                new[] { "Tour" });
+            }
+        }
     }
 }
