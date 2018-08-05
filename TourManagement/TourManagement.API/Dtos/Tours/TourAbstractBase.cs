e1250b09d6d9b924f962cf0f7c7da0154392dbe9
richie.buckingham <richie.buckingham@gmail.com>
2018-08-06 09:39:07 +1000

    add support for customised error messages

--- TourManagement/TourManagement.API/Dtos/Tours/TourAbstractBase.cs
+++ TourManagement/TourManagement.API/Dtos/Tours/TourAbstractBase.cs
@@ -8,15 +8,17 @@ namespace TourManagement.API.Dtos
 {
     public abstract class TourAbstractBase : IValidatableObject
     {
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Title is required.")]
-        [MaxLength(200, ErrorMessage = "Title is too long.")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "required|Title is required.")]
+        [MaxLength(200, ErrorMessage = "maxLength|Title is too long.")]
         public string Title { get; set; }
-        
-        [MaxLength(2000, ErrorMessage = "Description is too long.")]
+
+        [MaxLength(2000, ErrorMessage = "maxLength|Description is too long.")]
         public virtual string Description { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "required|The start date is required.")]
         public DateTimeOffset StartDate { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "required|The end date is required.")]
         public DateTimeOffset EndDate { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
@@ -24,9 +26,10 @@ namespace TourManagement.API.Dtos
             if (!(StartDate < EndDate))
             {
                 yield return new ValidationResult(
-                "The start date should be smaller than the end date.",
+                "startDateBeforeEndDate|The start date should be smaller than the end date.",
                 new[] { "Tour" });
             }
+
         }
     }
 }
--- TourManagement/TourManagement.API/Dtos/Tours/TourForUpdate.cs
+++ TourManagement/TourManagement.API/Dtos/Tours/TourForUpdate.cs
@@ -6,7 +6,7 @@ namespace TourManagement.API.Dtos
     public class TourForUpdate : TourAbstractBase
     {
         [Required(AllowEmptyStrings = false,
-            ErrorMessage = "When updating a tour, the description is required.")]
+            ErrorMessage = "required|When updating a tour, the description is required.")]
         public override string Description
         { get => base.Description; set => base.Description = value; }
     }
