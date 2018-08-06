e8e3232542602489856d8ab6da50a97ec89b89a5
richie.buckingham <richie.buckingham@gmail.com>
2018-08-06 17:26:48 +1000

    visualising errors on the client

--- TourManagement/TourManagement.Client/src/app/shared/handle-http-error-interceptor.ts
+++ TourManagement/TourManagement.Client/src/app/shared/handle-http-error-interceptor.ts
@@ -1,34 +1,42 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { HttpErrorResponse } from '@angular/common/http/src/response';
-import { GlobalErrorHandler } from './global-error-handler';
-
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { HttpErrorResponse } from "@angular/common/http/src/response";
+import { GlobalErrorHandler } from "./global-error-handler";
 
 @Injectable()
 export class HandleHttpErrorInterceptor implements HttpInterceptor {
+  constructor(private globalErrorHandler: GlobalErrorHandler) {}
 
-    constructor(private globalErrorHandler: GlobalErrorHandler) {
-    }
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).catch((error: HttpErrorResponse) => {
+      if (error.error instanceof Error) {
+        // client-side or network error
+        const errorToLog = `Http error (client/network). ${error.message}`;
+        this.globalErrorHandler.handleError(errorToLog);
+      } else {
+        // unsuccesful response code
+        const errorToLog = `Http error (unsuccessful reponse). Message: ${
+          error.message
+        }, status code: ${error.status}, body: ${JSON.stringify(error.error)} `;
+        this.globalErrorHandler.handleError(errorToLog);
+      }
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request)
-            .catch((error: HttpErrorResponse) => {
-                if (error.error instanceof Error) {
-                    // client-side or network error
-                    const errorToLog = `Http error (client/network). ${error.message}`;
-                    this.globalErrorHandler.handleError(errorToLog);
-                }
-                else {
-                    // unsuccesful response code                   
-                    const errorToLog = `Http error (unsuccessful reponse). Message: ${error.message}, status code: ${(error).status}, body: ${JSON.stringify(error.error)} `;
-                    this.globalErrorHandler.handleError(errorToLog);
-                }
-
-                return Observable.of(new HttpResponse());
-                
-            }
-            )
-    }
+      if (error.status === 422) {
+        // throw the error body
+        return Observable.throw(error.error);
+      } else {
+        return Observable.of(new HttpResponse());
+      }
+    });
+  }
 }
-
--- TourManagement/TourManagement.Client/src/app/tours/tour-add/tour-add.component.ts
+++ TourManagement/TourManagement.Client/src/app/tours/tour-add/tour-add.component.ts
@@ -1,110 +1,127 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { Band } from "../../shared/band.model";
-import { Manager } from "../../shared/manager.model";
-import { FormGroup, FormBuilder, FormArray, Validators } from "@angular/forms";
-import { Subscription } from "rxjs/Subscription";
-import { MasterDataService } from "../../shared/master-data.service";
-import { TourService } from "../shared/tour.service";
-import { Router } from "@angular/router";
-import { ShowSingleComponent } from "../shows/show-single/show-single.component";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Band } from '../../shared/band.model';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
+import { MasterDataService } from '../../shared/master-data.service';
+import { TourService } from '../shared/tour.service';
+import { Router } from '@angular/router';
+import { Manager } from '../../shared/manager.model';
+import { ShowSingleComponent } from '../shows/show-single/show-single.component';
+import { CustomValidators } from '../../shared/validators/custom-validators';
+import { ValidationErrorHandler } from '../../shared/validators/validation-error-handler';
 
 @Component({
-  selector: "app-tour-add",
-  templateUrl: "./tour-add.component.html",
-  styleUrls: ["./tour-add.component.css"]
+  selector: 'app-tour-add',
+  templateUrl: './tour-add.component.html',
+  styleUrls: ['./tour-add.component.css']
 })
-export class TourAddComponent implements OnInit, OnDestroy {
+export class TourAddComponent implements OnInit {
+
   public tourForm: FormGroup;
   bands: Band[];
   managers: Manager[];
-  public isAdmin: boolean = false;
+  private isAdmin: boolean = false;
 
-  constructor(
-    private masterDataService: MasterDataService,
+  constructor(private masterDataService: MasterDataService,
     private tourService: TourService,
     private formBuilder: FormBuilder,
-    private router: Router
-  ) {}
+    private router: Router) { }
 
   ngOnInit() {
+
     // define the tourForm (with empty default values)
     this.tourForm = this.formBuilder.group({
-      band: [""],
-      manager: [""],
-      title: ["", [Validators.required, Validators.maxLength(200)]],
-      description: [""],
-      startDate: [],
-      endDate: [],
+      band: ['', Validators.required],
+      manager: [''],
+      title: ['', [Validators.required, Validators.maxLength(200)]],
+      description: ['', Validators.maxLength(2000)],
+      startDate: [, Validators.required],
+      endDate: [, Validators.required],
       shows: this.formBuilder.array([])
-    });
+    }, { validator: CustomValidators.StartDateBeforeEndDateValidator });
 
     // get bands from master data service
-    this.masterDataService.getBands().subscribe(bands => {
-      console.log("bands = ", bands);
-      this.bands = bands;
-    });
+    this.masterDataService.getBands()
+      .subscribe(bands => {
+        this.bands = bands;
+      });
 
     if (this.isAdmin === true) {
       // get managers from master data service
-      this.masterDataService.getManagers().subscribe(managers => {
-        console.log("managers = ", managers);
-        this.managers = managers;
-      });
+      this.masterDataService.getManagers()
+        .subscribe(managers => {
+          this.managers = managers;
+        });
     }
   }
+
+  addShow(): void {
+    let showsFormArray = this.tourForm.get('shows') as FormArray;
+    // add show 
+    showsFormArray.push(ShowSingleComponent.createShow());
+  }
+
+  
+
 
   addTour(): void {
     if (this.tourForm.dirty && this.tourForm.valid) {
       if (this.isAdmin === true) {
         if (this.tourForm.value.shows.length) {
           let tour = automapper.map(
-            "TourFormModel",
-            "TourWithManagerAndShowsForCreation",
-            this.tourForm.value
-          );
-          this.tourService.addTourWithManagerAndShows(tour).subscribe(() => {
-            this.router.navigateByUrl("/tours");
-          });
-        } else {
-          let tour = automapper.map(
-            "TourFormModel",
-            "TourWithManagerForCreation",
-            this.tourForm.value
-          );
-          this.tourService.addTourWithManager(tour).subscribe(() => {
-            this.router.navigateByUrl("/tours");
-          });
+            'TourFormModel',
+            'TourWithManagerAndShowsForCreation',
+            this.tourForm.value);
+          this.tourService.addTourWithManagerAndShows(tour)
+            .subscribe(
+              () => {
+                this.router.navigateByUrl('/tours');
+              },
+              (validationResult) => 
+              { ValidationErrorHandler.handleValidationErrors(this.tourForm, validationResult); });
         }
-      } else {
+        else {
+          let tour = automapper.map(
+            'TourFormModel',
+            'TourWithManagerForCreation',
+            this.tourForm.value);
+          this.tourService.addTourWithManager(tour)
+            .subscribe(
+              () => {
+                this.router.navigateByUrl('/tours');
+              },
+              (validationResult) => 
+              { ValidationErrorHandler.handleValidationErrors(this.tourForm, validationResult); });
+        }
+      }
+      else {
         if (this.tourForm.value.shows.length) {
           let tour = automapper.map(
-            "TourFormModel",
-            "TourWithShowsForCreation",
-            this.tourForm.value
-          );
-          this.tourService.addTourWithShows(tour).subscribe(() => {
-            this.router.navigateByUrl("/tours");
-          });
-        } else {
+            'TourFormModel',
+            'TourWithShowsForCreation',
+            this.tourForm.value);
+          this.tourService.addTourWithShows(tour)
+            .subscribe(
+              () => {
+                this.router.navigateByUrl('/tours');
+              },
+              (validationResult) => 
+              { ValidationErrorHandler.handleValidationErrors(this.tourForm, validationResult); });
+        }
+        else {
           let tour = automapper.map(
-            "TourFormModel",
-            "TourForCreation",
-            this.tourForm.value
-          );
-          this.tourService.addTour(tour).subscribe(() => {
-            this.router.navigateByUrl("/tours");
-          });
+            'TourFormModel',
+            'TourForCreation',
+            this.tourForm.value);
+          this.tourService.addTour(tour)
+            .subscribe(
+              () => {
+                this.router.navigateByUrl('/tours');
+              },
+            (validationResult) => 
+            { ValidationErrorHandler.handleValidationErrors(this.tourForm, validationResult); });
         }
       }
     }
   }
-
-  addShow(): void {
-    console.log("in addShow()");
-    let showsFormsArray = this.tourForm.get("shows") as FormArray;
-    // add the show
-    showsFormsArray.push(ShowSingleComponent.createShow());
-  }
-
-  ngOnDestroy() {}
 }
