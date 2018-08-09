48bd5cd8f5875c5d605f99c877e17d79f27bdb38
richie.buckingham <richie.buckingham@gmail.com>
2018-08-09 10:51:34 +1000

    trigger signin when the app loads

--- TourManagement/TourManagement.Client/src/app/app.component.ts
+++ TourManagement/TourManagement.Client/src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-import {  } from "automapper-ts";
+import { Component, OnInit } from "@angular/core";
+import {} from "automapper-ts";
+import { OpenIdConnectService } from "./security/open-id-connect.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'Pluralsight Demo';  
+export class AppComponent implements OnInit {
+  title = "Pluralsight Demo";
+
+  constructor(private openIdConnectService: OpenIdConnectService) {}
+
+  ngOnInit(): void {
+    var path = window.location.pathname;
+    if (path != "/signin-oidc") {
+      if (!this.openIdConnectService.userAvailable) {
+        this.openIdConnectService.triggerSignIn();
+      }
+    }
+  }
 }
